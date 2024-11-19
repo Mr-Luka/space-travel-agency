b29f78928437f6f906fb454ad627b1eaa09e118d
Luka <luka.luka.luka93@gmail.com>
2024-11-19 12:52:17 -0800

    made a function that will handle the registration input, it has two regex variables that determine what is the correct name and email intake characters, captured the name and email value and input age, then I checked if any of those are filled and checked and send according alerts if not, then checked if any of it was filled, and then made an object that will contain all the registration info intake and put it into an object registration, so later on I can pull up the name and email the user used

--- script.js
+++ script.js
@@ -1,9 +1,51 @@
 const signUpBtn = document.querySelector('#form-open-btn');
 const welcomeContent = document.querySelector('.content');
 const signUpForm = document.querySelector('.sign-up-form')
+const nameInput = document.querySelector('input[name="name"]');
+const emailInput = document.querySelector('input[name="email"]');
+const signUpBtn2 = document.querySelector('button.btn2');
+console.log(nameInput);
+let name = [];
+let emailArray = [];
+let registration = {
+    fullName: '',
+    email: '',
+}
 
 signUpBtn.addEventListener('click', ()=> {
     welcomeContent.classList.add('hidden');
     signUpForm.classList.remove('hidden')
 })
 
+
+function handleSignUp (e) { 
+    e.preventDefault()
+    nameInput.innerHTML = "";
+    const acceptedName = /^[a-z ,.'-]+$/i;
+    const acceptedEmail = /^([a-zA-Z0-9._%-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,})$/;
+    const name = nameInput.value.trim();
+    const emailLower = (emailInput.value.trim()).toLowerCase();
+    const inputAge = document.querySelector('input[type="checkbox"]');
+    console.log(inputAge);
+
+    if (!acceptedName.test(name)){
+        alert('Please enter valid name')
+    }
+    if(!acceptedEmail.test(emailLower)){
+        alert('Please enter valid email')
+    }
+    if(!inputAge.checked){
+        alert('Please be 21 years or older')
+    }
+
+    if (!acceptedName.test(name) || !acceptedEmail.test(emailLower) || !inputAge.checked){
+        alert('Please fill out the registration form');
+    } else {
+        registration.fullName = name;
+        registration.email = emailLower;
+        console.log(registration);
+    }
+        
+
+}
+signUpBtn2.addEventListener('click', handleSignUp)
