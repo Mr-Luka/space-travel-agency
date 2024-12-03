aed0120b956225f26537394d66dd3bf2ba251a6c
Luka <luka.luka.luka93@gmail.com>
2024-12-02 21:56:59 -0800

    cleaning css code, cleaned home page

--- home.js
+++ home.js
@@ -14,9 +14,6 @@ function welcome (){
             <h2>Welcome to the Universe<br> ${firstName[0]}</h2>
             <p id="space"><span id="space-span">Space</span></p>
             <p class="cursor typewriter-animation">Join us ${registration.fullName} on a journey through the cosmos<br> and explore the wonders of the universe.</p>
-        </div>
-        <div class="explore-div">
-            <button class="explore" id="explore-btn">${firstName[0]}</button>
         </div>`;
     }
 }
